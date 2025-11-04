df2a5f6140d0216d22f57f6a180db8bac48d10c9
Alexey Sergeev <alexey@metriport.com>
2025-11-04 09:34:27 -0800

    feat(repo): address templates
    Ref ENG-000
    
    Signed-off-by: Alexey Sergeev <alexey@metriport.com>

--- commitlint.parserPreset.js
+++ commitlint.parserPreset.js
@@ -3,9 +3,8 @@ const teams = ["ENG", "OPS", "CS", "MKT", "SLS"];
 const prefixes = ["Ref", "References", "Part of", "Fixes", "Closes"];
 
 const issuePrefixes = [
-  " #", // TODO REMOVE THIS ASAP, this is a temporary fix to allow commitlint to work with existing commits that reference GH issues
-  ...prefixes.flatMap(prefix => {
-    return teams.map(team => `${prefix} ${team}-`);
+  ...prefixes.flatMap((prefix) => {
+    return teams.map((team) => `${prefix} ${team}-`);
   }),
 ];
 
